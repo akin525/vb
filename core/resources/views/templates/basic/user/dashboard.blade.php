f23a22d068238d0fc13fa5b770f7652c4df5276a
akina <akinlabisamson15@gmail.com>
2025-01-23 10:40:45 +0100

    done

--- core/resources/views/templates/basic/user/dashboard.blade.php
+++ core/resources/views/templates/basic/user/dashboard.blade.php
@@ -457,7 +457,7 @@
                         </a>
                     </div>
                     <div class="col-4 col-sm-3 col-lg-3">
-                        <a href="{{url('user/cabltv')}}">
+                        <a href="{{url('user/cabletv')}}">
                             <div class="card">
                                 <div class="card-body p-3 text-center">
                              <span style="font-size: 30px;">
